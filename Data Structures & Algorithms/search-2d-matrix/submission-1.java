a637911eef3a446fc39c5a6f61c2b03d81529dd4
Philip Kalela <33759757+Kalela@users.noreply.github.com>
2026-09-16 19:30:14 +0300

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int[] possibleMatch = null;
+
+        int l = 0, r = matrix.length - 1;
+        int pivot1 = l + ((r - l) / 2);
+
+        while (l <= r) {
+            int pivot = l + ((r - l) / 2);
+            if (matrix[pivot][0] == target) {
+                return true;
+            }
+            if (matrix[pivot][0] < target && matrix[pivot][matrix[pivot].length - 1] < target) {
+                l = pivot + 1;
+            }
+
+            if (matrix[pivot][0] > target && matrix[pivot][matrix[pivot].length - 1] > target) {
+                r = pivot - 1;
+            }
+            if (matrix[pivot][0] < target && matrix[pivot][matrix[pivot].length - 1] >= target) {
+                
+                possibleMatch = matrix[pivot];
+                break;
+            }
+        }
+
+        System.out.println("Possible match " + Arrays.toString(possibleMatch));
+
+        if (possibleMatch == null) {
+            return false;
+        }
+
+        l = 0;
+        r = possibleMatch.length - 1;
+
+        while(l <= r) {
+            int pivot = l + ((r - l) / 2);
+            if (possibleMatch[pivot] == target) {
+                return true;
+            }
+
+            if (possibleMatch[pivot] > target) {
+                r = pivot - 1;
+            }
+
+            if (possibleMatch[pivot] < target) {
+                l = pivot + 1;
+            }
+        }
+
+        return false;
+    }
+}
