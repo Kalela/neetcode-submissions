f438c3749ec586b1e436853324ba67e88d37d33c
Philip Kalela <33759757+Kalela@users.noreply.github.com>
2026-09-17 08:29:32 +0300

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int numberOfRotations = nums.length;
+        boolean wasAlreadySorted = true;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > nums[i + 1]) {
+                numberOfRotations = i + 1;
+                wasAlreadySorted = false;
+                break;
+            }
+        }
+
+        Arrays.sort(nums);
+
+        int number = -1;
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int pivot = l + ((r - l) / 2);
+            if (nums[pivot] == target) {
+                number = pivot;
+                break;
+            }
+            if (nums[pivot] > target) {
+                r = pivot - 1;
+            }
+
+            if (nums[pivot] < target) {
+                l = pivot + 1;
+            }
+        }
+
+        if (wasAlreadySorted) {
+            return number;
+        }
+
+        System.out.println("wasAlreadySorted " + wasAlreadySorted);
+        System.out.println("numberOfRotations " + numberOfRotations);
+
+
+        return number != -1 ? number + numberOfRotations : number;
+    }
+}
